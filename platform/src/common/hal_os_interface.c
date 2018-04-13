261105c4c7b59a899e3510506d58453c8f008677
chenkaiyao <chenkaiyao@molmc.com>
2018-04-13 18:08:58 +0800

    1. platform添加common目录

---
+++ platform/src/common/hal_os_interface.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2013-2018 Molmc Group. All rights reserved.
+ * License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "hal_import.h"
+
+void *HAL_MutexCreate(void)
+{
+    return NULL;
+}
+
+void HAL_MutexDestroy(void *mutex)
+{
+}
+
+void HAL_MutexLock(void *mutex)
+{
+}
+
+void HAL_MutexUnlock(void *mutex)
+{
+}
+
+void *HAL_Malloc(uint32_t size)
+{
+    return NULL;
+}
+
+void HAL_Free(void *ptr)
+{
+}
+
+void HAL_SystemReboot(void)
+{
+}
+
+uint32_t HAL_UptimeMs(void)
+{
+    return 0;
+}
+
+void HAL_SleepMs(uint32_t ms)
+{
+}
+
+void HAL_Srandom(uint32_t seed)
+{
+}
+
+uint32_t HAL_Random(uint32_t region)
+{
+    return 0;
+}
+
+void HAL_Printf(const char *fmt, ...)
+{
+}
+
+int HAL_Snprintf(char *str, const int len, const char *fmt, ...)
+{
+    return 0;
+}
+
+int HAL_Vsnprintf(char *str, const int len, const char *format, va_list ap)
+{
+    return 0;
+}
+
---
+++ platform/src/common/hal_tcp_interface.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2013-2018 Molmc Group. All rights reserved.
+ * License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "hal_import.h"
+
+uintptr_t HAL_TCP_Establish(const char *host, uint16_t port)
+{
+    return 0;
+}
+
+int HAL_TCP_Destroy(uintptr_t fd)
+{
+    return 0;
+}
+
+int32_t HAL_TCP_Write(uintptr_t fd, const char *buf, uint32_t len, uint32_t timeout_ms)
+{
+    return 0;
+}
+
+int32_t HAL_TCP_Read(uintptr_t fd, char *buf, uint32_t len, uint32_t timeout_ms)
+{
+    return 0;
+}
+
